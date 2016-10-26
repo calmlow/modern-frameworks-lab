4e9208cb9abc2c484fb5639caa3719ca25de7c0d
Michael Medin <michael@medin.name>
2016-10-26 21:37:04 +0200

    6.5 Add markdown rendering

--- scripts/controllers/list.js
+++ scripts/controllers/list.js
@@ -4,6 +4,10 @@ angular.module('githubView')
   $scope.items = [];
   $scope.refresh = function() {
     ReleaseService.query(function(data) {
+      var converter = new showdown.Converter();
+      data.forEach(function (e) {
+        e.desc = converter.makeHtml(e.body);
+      })
       $scope.items = data;
     })
   }
