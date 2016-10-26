d01574517d41b2755526ce0cd527922669ab7b0b
Michael Medin <michael@medin.name>
2016-10-26 20:36:02 +0200

    3.1 Create a view

--- scripts/app.js
+++ scripts/app.js
@@ -5,4 +5,15 @@ angular.module('githubView',[
 	'ngSanitize',
 	'ui.bootstrap'
 	])
+	.config(function ($routeProvider) {
+		$routeProvider
+		.when('/', {
+			templateUrl: 'views/list.html',
+			controller: 'ListController',
+			controllerAs: 'list'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	})
 	;
