4861ae72950ad5d9ff50f82d85f280d45d1a2f8e
Michael Medin <michael@medin.name>
2016-10-26 21:15:55 +0200

    5.2 Implement a refresh button

--- scripts/controllers/list.js
+++ scripts/controllers/list.js
@@ -2,4 +2,7 @@ angular.module('githubView')
 .controller('ListController', function($scope, ReleaseService) {
   $scope.searchValue = "";
   $scope.items = ReleaseService.query()
+  $scope.refresh = function() {
+    $scope.items = ReleaseService.query()
+  }
 });
